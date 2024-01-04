b69ac4489b14e59ebb4638ea05912e729cf3aee6
Millad Ayoubi <Milladayoubi@gmail.com>
2024-01-04 15:13:23 -0500

    Chart Box

---
+++ src/components/chartBox/chartBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const chartBox = () => {
+  return <div>chartBox</div>;
+};
+
+export default chartBox;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./home.scss";
 import TopBox from "../../components/topBox/TopBox";
+import ChartBox from "../../components/chartBox/chartBox";
+
 const Home = () => {
   return (
     <section className="home">
@@ -8,11 +10,20 @@ const Home = () => {
         <TopBox />
       </div>
 
-      <div className="box box2">Box 2</div>
-      <div className="box box3">Box 3</div>
+      <div className="box box2">
+        <ChartBox />
+      </div>
+      <div className="box box3">
+        {" "}
+        <ChartBox />
+      </div>
       <div className="box box4">Box 4</div>
-      <div className="box box5">Box 5</div>
-      <div className="box box6">Box 6</div>
+      <div className="box box5">
+        <ChartBox />
+      </div>
+      <div className="box box6">
+        <ChartBox />
+      </div>
       <div className="box box7">Box 7</div>
       <div className="box box8">Box 8</div>
       <div className="box box9">Box 9</div>
