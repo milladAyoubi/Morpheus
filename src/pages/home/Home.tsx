7e3fe15af1bdc906a353bfb3b85c5bc069441817
Millad Ayoubi <Milladayoubi@gmail.com>
2024-01-06 00:53:24 -0500

    Dashboard Update

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,31 +2,36 @@ import React from "react";
 import "./home.scss";
 import TopBox from "../../components/topBox/TopBox";
 import ChartBox from "../../components/chartBox/chartBox";
-
+import Clock from "react-live-clock";
 const Home = () => {
   return (
-    <section className="home">
-      <div className="box box1">
-        <TopBox />
+    <section>
+      <div className="title">
+        <h1>DASHBOARD</h1>
+        <p>Welcome to your dashboard!</p>
       </div>
-
-      <div className="box box2">
-        <ChartBox />
+      <div className="home">
+        <div className="box box1">
+          <TopBox />
+        </div>
+        <div className="box box2">
+          <ChartBox />
+        </div>
+        <div className="box box3">
+          {" "}
+          <ChartBox />
+        </div>
+        <div className="box box4">Box 4</div>
+        <div className="box box5">
+          <ChartBox />
+        </div>
+        <div className="box box6">
+          <ChartBox />
+        </div>
+        <div className="box box7">Box 7</div>
+        <div className="box box8">Box 8</div>
+        <div className="box box9">Box 9</div>
       </div>
-      <div className="box box3">
-        {" "}
-        <ChartBox />
-      </div>
-      <div className="box box4">Box 4</div>
-      <div className="box box5">
-        <ChartBox />
-      </div>
-      <div className="box box6">
-        <ChartBox />
-      </div>
-      <div className="box box7">Box 7</div>
-      <div className="box box8">Box 8</div>
-      <div className="box box9">Box 9</div>
     </section>
   );
 };
